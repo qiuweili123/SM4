01ecaad133ef35b2abed1f74b477eee8dbfe144b
liqiuwei <qiuweili123@163.com>
2018-04-27 23:41:30 +0800

    add file

--- src/test/java/com/drools/BaseFunTest.java
+++ src/test/java/com/drools/BaseFunTest.java
@@ -1,10 +1,10 @@
 package com.drools;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.kie.api.runtime.KieSession;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class BaseFunTest extends BaseTest {
 
@@ -15,4 +15,36 @@ public class BaseFunTest extends BaseTest {
         execute(session, map);
     }
 
+    @Test
+    public void testContains(){
+
+        KieSession session = getKieSession("String_Contains");
+        Map<String, Object> map = new HashMap<>();
+        map.put("code","101");
+        execute(session, map);
+        System.out.println("##map=="+map);
+
+    }
+@Test
+    public void testFrom(){
+
+        KieSession session = getKieSession("String_From");
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("code","101");
+        execute(session, map);
+        System.out.println("##map=="+map);
+
+    }
+    @Test
+    public void testIN(){
+
+        KieSession session = getKieSession("String_IN");
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("code","10");
+        execute(session, map);
+        System.out.println("##map=="+map);
+
+    }
 }
