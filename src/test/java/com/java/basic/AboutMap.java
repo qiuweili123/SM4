985715d72a2b9b6e7039fa7ba098fa1f64ef2901
liqiuwei <qiuweili123@163.com>
2018-04-29 17:55:10 +0800

    add

--- src/test/java/com/java/basic/AboutMap.java
+++ src/test/java/com/java/basic/AboutMap.java
@@ -1,4 +1,18 @@
 package com.java.basic;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class AboutMap {
+    public static void main(String[] args) {
+
+        Map<String, Object> detailInfo = new LinkedHashMap<>();
+        detailInfo.put("最小储值",30);
+        detailInfo.put("最大储值",60);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, Object> entry : detailInfo.entrySet()) {
+            stringBuilder.append(entry.getKey()).append(":").append(entry.getValue()).append("    ");
+        }
+        System.out.println(stringBuilder);
+    }
 }
