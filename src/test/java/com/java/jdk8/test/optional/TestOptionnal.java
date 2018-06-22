084e63baeced929da33ac329e69613df6ab158c6
liqiuwei <qiuweili123@163.com>
2018-06-22 23:28:51 +0800

    add

--- src/test/java/com/java/jdk8/test/optional/TestOptionnal.java
+++ src/test/java/com/java/jdk8/test/optional/TestOptionnal.java
@@ -1,5 +1,7 @@
 package com.java.jdk8.test.optional;
 
+import com.commons.test.User;
+
 import java.util.Optional;
 
 /**
@@ -12,5 +14,16 @@ public class TestOptionnal {
         System.out.println("Full Name is set? " + fullName.isPresent());
         System.out.println("Full Name: " + fullName.orElseGet(() -> "[none]"));
         System.out.println(fullName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
+        System.out.println(fullName.orElse(null));
+           User user=null;
+        User u2 = Optional.ofNullable(user).orElseGet(() -> {
+           User u = new User();
+            u.setAge(30);
+            return u;
+        });
+        System.out.println("##"+u2.getAge());
+        //当u2不为空时执行特定操作
+         Optional.ofNullable(u2).ifPresent(u->{u.setName("zhangsan");});
+        System.out.println(u2.getName());
     }
 }
